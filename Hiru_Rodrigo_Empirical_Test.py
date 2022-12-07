37c0083a7bd717ec96fddf9dd8840090f766c7b1
bocrodh <111758660+bocrodh@users.noreply.github.com>
2022-12-07 13:57:34 -0500

    Update Hiru_Rodrigo_Empirical_Test.py

--- Hiru_Rodrigo_Empirical_Test.py
+++ Hiru_Rodrigo_Empirical_Test.py
@@ -7,7 +7,7 @@ Subject: Empirical Test                 "
 #################################################################### Code outline #########################################################################
 "Section 1: Library imports 
 "Section 2: Data cleaning and verifications
-"Section 3: Data manipulation 
+"Section 3: Data manipulation/Plots 
 "Section 4: Dashboard 
 
 ###################################################################### Library imports ####################################################################
@@ -36,9 +36,39 @@ df = df["AAA","BBB"]
 #Format date object 
 df['date'] = pd.to_datetime(df["date"].dt.strftime('%Y-%m'))
 
-###################################################################### Data manipulation ##################################################################
+###################################################################### Data manipulation/Plots ##################################################################
 
+#Question 1 Plot: 
+fig = px.line(cost, x="Year", y='%', color="Country", title="Remittance Fee as % of $200 <br><sup>Source: https://remittanceprices.worldbank.org/</sup>")
+fig.update_traces(mode="lines")
+fig.update_layout(hovermode="x unified") 
+fig.update_xaxes(tickangle= -90, nticks=4)
+fig.update_traces(patch={"line": {"color": "purple", "width": 6}}, selector={"legendgroup": "Canada"})
+fig.update_traces(patch={"line": {"color": "MidnightBlue", "width": 6, "dash": 'dot'}}, selector={"legendgroup": "G8"}) 
+fig.show() 
 
+#Question 2 Plot:  
+fig2 = px.line(times, x="Year", y='Number of Service Providers', color="Country", title="Access to <1 Hour Remittance Services (by corridor)<br><sup>Source: https://remittanceprices.worldbank.org/</sup>")
+fig2.update_traces(mode="lines")
+fig2.update_layout(hovermode="x unified") 
+fig2.update_xaxes(tickangle= -90, nticks=4)
+fig2.show() 
+
+#Question 3 Plot:  
+fig3 = px.line(banked, x='Year', y='% Banked', color="Country", title="% of Banked Population <br><sup>Source: https://databank.worldbank.org/source/global-financial-inclusion/</sup>")
+fig3.update_traces(mode="lines")
+fig3.update_layout(hovermode="x unified") 
+fig3.update_xaxes(tickangle= -90)
+fig3.update_traces(patch={"line": {"color": "purple", "width": 6}}, selector={"legendgroup": "Canada"})
+fig3.update_traces(patch={"line": {"color": "MidnightBlue", "width": 6, "dash": 'dot'}}, selector={"legendgroup": "G8"}) 
+fig3.show() 
+
+#Question 4 Plot:  
+fig4 = px.line(provider, x='Year', y='%', color="Country", title="% of Remittances Completed Money Transfer Services (by corridor) <br><sup>Source: https://remittanceprices.worldbank.org/</sup>")
+fig4.update_traces(mode="lines")
+fig4.update_layout(hovermode="x unified") 
+fig4.update_xaxes(tickangle= -90, nticks=4)
+fig4.show() 
 
 ###################################################################### Dashboard ############################################################################
 def main(): 
@@ -48,16 +78,14 @@ def main():
 
     #Customizing sidebar filters 
     st.sidebar.header("Filter Criteria:") 
-    
     bb_number = st.sidebar.multiselect("BB Number:", options=df["BB Number"].unique(), default=df['BB Number'].unique(),)  
-    
     df_selection = df.query("`BB Number` ==@bb_number") 
     
-    #Table 1
+    #Displaying Dataframe 
     st.header('Table 1')
     filtered = st.multiselect("Filter fields", options=df_selection.columns, default=['BB Number'])
  
-    #Editing Table 1 appearance 
+    #Editing DataFrame Appearance  
     fig = go.Figure(data=[go.Table(
     header=dict(values=list(filtered),
       fill_color='#575a5e',
@@ -79,38 +107,8 @@ def main():
     """,
     unsafe_allow_html=True,) 
 
-    #Plots     
+    #Displaying Plots      
     st.header('Plots')
-    
-    fig = px.line(cost, x="Year", y='%', color="Country", title="Remittance Fee as % of $200 <br><sup>Source: https://remittanceprices.worldbank.org/</sup>")
-    fig.update_traces(mode="lines")
-    fig.update_layout(hovermode="x unified") 
-    fig.update_xaxes(tickangle= -90, nticks=4)
-    fig.update_traces(patch={"line": {"color": "purple", "width": 6}}, selector={"legendgroup": "Canada"})
-    fig.update_traces(patch={"line": {"color": "MidnightBlue", "width": 6, "dash": 'dot'}}, selector={"legendgroup": "G8"}) 
-    fig.show() 
-    #st.plotly_chart(fig, use_container_width=True)
-
-    fig2 = px.line(times, x="Year", y='Number of Service Providers', color="Country", title="Access to <1 Hour Remittance Services (by corridor)<br><sup>Source: https://remittanceprices.worldbank.org/</sup>")
-    fig2.update_traces(mode="lines")
-    fig2.update_layout(hovermode="x unified") 
-    fig2.update_xaxes(tickangle= -90, nticks=4)
-    fig2.show() 
-    #st.plotly_chart(fig2, use_container_width=True)
-
-    fig3 = px.line(banked, x='Year', y='% Banked', color="Country", title="% of Banked Population <br><sup>Source: https://databank.worldbank.org/source/global-financial-inclusion/</sup>")
-    fig3.update_traces(mode="lines")
-    fig3.update_layout(hovermode="x unified") 
-    fig3.update_xaxes(tickangle= -90)
-    fig3.update_traces(patch={"line": {"color": "purple", "width": 6}}, selector={"legendgroup": "Canada"})
-    fig3.update_traces(patch={"line": {"color": "MidnightBlue", "width": 6, "dash": 'dot'}}, selector={"legendgroup": "G8"}) 
-    fig3.show() 
-    
-    fig4 = px.line(provider, x='Year', y='%', color="Country", title="% of Remittances Completed Money Transfer Services (by corridor) <br><sup>Source: https://remittanceprices.worldbank.org/</sup>")
-    fig4.update_traces(mode="lines")
-    fig4.update_layout(hovermode="x unified") 
-    fig4.update_xaxes(tickangle= -90, nticks=4)
-    fig4.show() 
     
     plot1, plot2 = st.columns(2,gap='small')
     plot1.plotly_chart(fig, use_container_width=True)    
