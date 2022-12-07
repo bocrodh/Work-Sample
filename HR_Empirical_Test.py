1573f35695dd33bf430e2626caedc17ccc8036e9
bocrodh <111758660+bocrodh@users.noreply.github.com>
2022-12-07 14:48:13 -0500

    Update HR_Empirical_Test.py

--- HR_Empirical_Test.py
+++ HR_Empirical_Test.py
@@ -20,37 +20,33 @@ import pickle
 from pathlib import Path 
 ############################################################# Data cleaning and verifications ##############################################################
 #Read in data
-#url = (r'https://github.com/bocrodh/21eB6-WCN-Ao-v-JK-yRpRT0e-qp/blob/main/CB%20BB%20Dashboard%20for%20Canada%20(September%202022).csv?raw=true') 
-#df = pd.read_csv(url, header=0)
+url = (r'https://github.com/bocrodh/21eB6-WCN-Ao-v-JK-yRpRT0e-qp/blob/main/CB%20BB%20Dashboard%20for%20Canada%20(September%202022).csv?raw=true') 
+df = pd.read_csv(url, header=0)
 
 #Sort data
-#df.sort_values("date", inplace=True)
+df = df.sort_values("date", inplace=True)
 
 #Delete duplicate rows and only keep first instance
-#df = df.drop_duplicates(keep='first')
+df = df.drop_duplicates(keep='first')
 
 #Subset relevant fields for analysis"
-#df = df["AAA","BBB"]
+df = df["AAA","BBB"]
 
 #Format date object 
-#df['date'] = pd.to_datetime(df["date"].dt.strftime('%Y-%m'))
+df['date'] = pd.to_datetime(df["date"].dt.strftime('%Y-%m'))
 
-xls = (r'https://github.com/bocrodh/21eB6-WCN-Ao-v-JK-yRpRT0e-qp/blob/main/wb_remittance_data.xlsx?raw=true')
+###################################################################### Data manipulation/Plots ###############################################################
 
 cost = pd.read_excel(xls, 'Cost of sending $200', header=0)
 times = pd.read_excel(xls, '<1 Hour Options', header=0)
 provider = pd.read_excel(xls, 'Proportion of MT', header=0)
 banked = pd.read_excel(xls, 'Banked', header=0)
 
-
-###################################################################### Data manipulation/Plots ###############################################################
-
 #Question 1 Plot: 
 fig = px.line(cost, x="Year", y='%', color="Country", title="Remittance Fee as % of $200")
 fig.update_traces(mode="lines")
 fig.update_layout(hovermode="x unified") 
 fig.update_xaxes(tickangle= -90, nticks=4)
-fig.update_traces(patch={"line": {"color": "purple", "width": 6}}, selector={"legendgroup": "Canada"})
 fig.update_traces(patch={"line": {"color": "MidnightBlue", "width": 6, "dash": 'dot'}}, selector={"legendgroup": "G8"}) 
 fig.show() 
 
@@ -80,11 +76,39 @@ fig4.show()
 ###################################################################### Dashboard ############################################################################
 def main(): 
                
-    #Create blank webpage 
+ #Create blank webpage 
     st.set_page_config(page_icon=":money_with_wings:", layout="wide",) 
 
     #Customizing sidebar filters 
     st.sidebar.header("Filter Criteria:") 
+    bb_number = st.sidebar.multiselect("BB Number:", options=df["BB Number"].unique(), default=df['BB Number'].unique(),)  
+    df_selection = df.query("`BB Number` ==@bb_number") 
+    
+    #Displaying Dataframe 
+    st.header('Table 1')
+    filtered = st.multiselect("Filter fields", options=df_selection.columns, default=['BB Number'])
+ 
+    #Editing DataFrame Appearance  
+    fig = go.Figure(data=[go.Table(
+    header=dict(values=list(filtered),
+      fill_color='#575a5e',
+      align='center', 
+      font=dict(color='white', size=15)),
+    cells=dict(values=[df_selection[col] for col in filtered],
+     fill_color='#ffffff',
+     align='center', 
+     font=dict(color='black', size=13)))])
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=450)
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.markdown(
+    """
+    <style>
+    span[data-baseweb="tag"] {background-color: #7E7F7A !important;}  
+    span[data-baseweb="tag"]>span{font-size: 13px}  
+    </style>
+    """,
+    unsafe_allow_html=True,) 
 
     #Displaying Plots      
     st.header('Plots')
@@ -96,5 +120,6 @@ def main():
     plot3, plot4 = st.columns(2,gap='small')
     plot3.plotly_chart(fig3, use_container_width=True)    
     plot4.plotly_chart(fig4, use_container_width=True)
+
 main()     
 #END 
